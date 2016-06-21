ee3821a5f8b81c325099ff16873337ba9b6fa2d8
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-21 15:21:16 -0700

    tried to implement another ajax query

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -1,7 +1,9 @@
 (function(module) {
   var repos = {};
+  var user = {};
 
   repos.allRepos = [];
+  user.allUser = [];
 
   repos.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
@@ -18,11 +20,33 @@
     });
   };
 
+  user.requestUser = function(callback) {
+    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+      Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/meganreardon',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        console.log(data);
+        repos.allUser = data;
+        callback();
+      }
+    });
+  };
+
   repos.withTheAttribute = function(myAttr) {
     return repos.allRepos.filter(function(aRepo) {
       return aRepo[myAttr];
     });
   };
 
+  user.withTheAttribute = function(myAttr) {
+    return user.allUser.filter(function(aRepo) {
+      return aRepo[myAttr];
+    });
+  };
+
   module.repos = repos;
+  module.user = user;
 })(window);
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -1,12 +1,18 @@
 (function(module) {
   var repoView = {};
+  var userView = {};
   /* TODO: Let's compile our new template!
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
   var repoCompiler = function(repo) {
     var template = Handlebars.compile($('#repo-template').text());
     return template(repo);
-  };  // Finish the Handlebars method here!
+  };
+
+  var userCompiler = function(user) {
+    var template = Handlebars.compile($('#githubInfo').text());
+    return template(user);
+  };// Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
@@ -14,11 +20,17 @@
       .map(repoCompiler)
     );
   };
+
+  userView.renderUser = function() {
+    $('#github-info').append(user.allUser.userCompiler);
+  };
 /* TODO: Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
   repos.requestRepos(repoView.renderRepos);
+  user.requestUser(userView.renderUser);
 
   module.repoView = repoView;
+  module.userView = userView;
 
 })(window);
